4f0b2d368e1e2f098e5a3f5eb4b1c241821936f5
github username <github email address>
2019-02-25 00:21:08 -0500

    added listenForClick & getEvents

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,5 +1,25 @@
+function listenForClick() {
+  $('button#post-data').on('click', function(event) {
+    event.preventDefault()
+    getEvent()
+  })
 
+  Function getEvents() {
+  $.ajax({
+    url: 'http://localhost:3000/users/2/events',
+    method: 'get'
+    type: 'json'
+  }).done(function(data) {
 
+    console.log("the data is: ", data)
+    debugger
+    let myEvent = new Event(data[0])
+    let myEventHtml = myEvent.eventHTML()
+    document.getElementById('ajax-post').innerHTML += myEventHTML
+  })
+}
+
+}
 class Event {
   constructor(obj) {
     this.id = obj.id
